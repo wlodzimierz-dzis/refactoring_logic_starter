6a769e8e1858d981279d48a2ede21912bb892e56
kwasniew <kwasniewski.mateusz@gmail.com>
2019-11-20 10:25:23 +0100

    normal update

--- src/shop.js
+++ src/shop.js
@@ -3,8 +3,25 @@ class Shop {
         this.items = items;
     }
 
+    // decrease sellIn by 1
+    // decrease quality by 1 when sellIn > 0
+    // decrease quality by 2 when sellIn <= 0
+    // never drop quality below 0
+    normalUpdate(item) {
+        if(item.sellIn > 0) item.quality -= 1;
+        if(item.sellIn <= 0) item.quality -= 2;
+        if(item.quality < 0) item.quality = 0;
+        item.sellIn -= 1;
+    }
+
     updateQuality() {
         this.items.forEach(item => {
+
+            // seam
+            if(item.name === "+5 Dexterity Vest") {
+                return this.normalUpdate(item);
+            }
+
             if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (item.quality > 0) {
                     if (item.name != 'Sulfuras, Hand of Ragnaros') {
