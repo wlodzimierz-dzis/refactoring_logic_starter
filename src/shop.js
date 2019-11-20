739aa260a92397324a809b73494d2450ae408bf6
kwasniew <kwasniewski.mateusz@gmail.com>
2019-11-20 10:37:19 +0100

    all update seams

--- src/shop.js
+++ src/shop.js
@@ -21,6 +21,19 @@ class Shop {
         item.sellIn -= 1;
     }
 
+    sulfurasUpdate(item) {
+
+    }
+
+    backstagePassUpdate(item) {
+        item.quality += 1;
+        if(item.sellIn <= 10) item.quality += 1;
+        if(item.sellIn <= 5) item.quality += 1;
+        if(item.quality > 50) item.quality = 50;
+        if(item.sellIn <= 0) item.quality = 0;
+        item.sellIn -= 1;
+    }
+
     updateQuality() {
         this.items.forEach(item => {
 
@@ -30,6 +43,12 @@ class Shop {
             }
             if(item.name === "Aged Brie") {
                 return this.brieUpdate(item);
+            }
+            if(item.name === "Sulfuras, Hand of Ragnaros") {
+                return this.sulfurasUpdate(item);
+            }
+            if(item.name === "Backstage passes to a TAFKAL80ETC concert") {
+                return this.backstagePassUpdate(item);
             }
 
             if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
